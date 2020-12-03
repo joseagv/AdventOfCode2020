23069dedb00fdca604a4a32c39cd0c34bc66a4fb
jose_x_744 <jose_x_744@hotmail.com>
2020-12-03 03:49:45 +0100

    Day one part 2 done.

--- AdventOfCode2020/src/aoc/day01/ReportRepair.java
+++ AdventOfCode2020/src/aoc/day01/ReportRepair.java
@@ -10,7 +10,7 @@ public class ReportRepair {
 		File file = new File(src);
 		Scanner sc = new Scanner(file);
 		final int myTarget = 2020;
-		int solution;
+		int solution, solution2, num3;
 		boolean found = false;
 		
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
@@ -22,12 +22,30 @@ public class ReportRepair {
 		sc.close();
 		file.exists();
 		
+		//Part One
 		for(int num1 : numbers) {
 			for(int num2: numbers) {
-				if(num1 == num2 || num2 == num1) continue;
+				if(num1 == num2) continue;
 				if(myTarget == num1 + num2) {
 					solution = num1 * num2;
-					System.out.println("\tnum1 = " + num1 +"\n\tnum2 = " + num2 + "\n\tSolution = " + solution);
+					System.out.println("Part 1:\n\tnum1 = " + num1 +"\n\tnum2 = " + num2 + "\n\tSolution = " + solution);
+					found = true;
+					break;
+				}
+			}
+			if(found) break;
+		}
+		found = false;
+		
+		//Part Two
+		for(int num1 : numbers) {
+			for(int num2: numbers) {
+				if(num1 == num2) continue;
+				num3 = myTarget - num1 - num2;
+				if(num3 == num1 || num3 == num2) continue;
+				if(numbers.contains(num3)) {
+					solution2 = num1 * num2 * num3;
+					System.out.println("\nPart 2:\n\tnum1 = " + num1 +"\n\tnum2 = " + num2 + "\n\tnum3 = " + num3 + "\n\tSolution = " + solution2);
 					found = true;
 					break;
 				}
