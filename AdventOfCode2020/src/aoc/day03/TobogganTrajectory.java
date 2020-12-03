d37e27c4bc066f252f22bf1487c12817c4be7b64
jose_x_744 <jose_x_744@hotmail.com>
2020-12-03 22:17:39 +0100

    Second part of day two done.

--- AdventOfCode2020/src/aoc/day03/TobogganTrajectory.java
+++ AdventOfCode2020/src/aoc/day03/TobogganTrajectory.java
@@ -20,21 +20,24 @@ public class TobogganTrajectory {
 		System.out.println("Part one count:" + count);
 		
 		//Part two
-		int result = slope(1, 1, numRows, numCols, myListString) *
+		long result = (long) slope(1, 1, numRows, numCols, myListString) *
 				slope(3, 1, numRows, numCols, myListString) *
 				slope(5, 1, numRows, numCols, myListString) *
 				slope(7, 1, numRows, numCols, myListString) *
 				slope(1, 2, numRows, numCols, myListString);
 		System.out.println("Part two count:" + result);
+		//System.out.println(slope(3, 1, numRows, numCols, myListString)); //Checks the slope from Part one. 
 	}
 	
-	private static int slope(int posX, int posY, int numRows, int numCols, ArrayList<String> myListString) {
-		int count = 0;
-		for(int i = posY; i < numRows - 1; i = i + posY) {	
-			count += (myListString.get(i + 1).charAt(posX) == '#') ? 1 : 0 ;
-			posX = (posX + 3 > numCols - 1) ? posX + 3 - numCols : posX + 3;
+	private static int slope(int xPos, int yPos, int numRows, int numCols, ArrayList<String> myListString) {
+		int count = 0, posX = xPos;
+		for(int i = 0; i < numRows - yPos;  i = i + yPos) {	
+			count += (myListString.get(i + yPos).charAt(posX) == '#') ? 1 : 0 ;
+			posX = (posX + xPos > numCols - 1) ? posX + xPos - numCols : posX + xPos;
 		}
 		return count;
 	}
+	
+
 
 }
